72153f5252e3fd3064f4f30bf255cccf2daa7bc0
AnXBui <axb24@drexel.edu>
2019-09-04 22:55:56 -0400

    Final (Responsive Navigation)

--- An Web2/Dev/js/scripts.js
+++ An Web2/Dev/js/scripts.js
@@ -1,44 +1,74 @@
-var $burger = $("#burger-icon");
+var $burger = $("#burger-container");
 var $nav = $('nav[data-nav]');
 var $navA = $('nav[data-nav] a');
 var isVisible = false;
-
-function mouseClick() {
-
-  console.log("click");
-  if (isVisible === false) {
-    $nav.show();
-    isVisible = true;
-  } else {
-    $nav.hide();
-    isVisible = false;
-  }
-}
-
-$burger.on("click", mouseClick);
-// close the mobile menu when menu link is clicked
-$navA.on("click", mouseClick);
-
-
+var $topLine = $('#burger-container div:nth-child(1)');
+var $middleLine = $('#burger-container div:nth-child(2)');
+var $bottomLine = $('#burger-container div:nth-child(3)');
+var animationSpeed = 0.25;
 
 var $searchButton = $('#search-button');
 var $searchTrigger = $('#search-trigger');
 var searchVisible = false;
 
+// ============================ //
+//         MENU + BURGER        //
+// ============================ //
 
-function searchBar() {
-  console.log("click");
-  if (searchVisible === false) {
-    $searchButton.addClass("expanded");
-    searchVisible = true;
+function mouseClick() {
+
+  console.log('mouseClick');
+  if (isVisible === false) {
+    $nav.show();
+    $nav.toggleClass("nav-expanded");
+    TweenMax.to($topLine, animationSpeed, {
+      rotation: 45,
+      y: 10
+    });
+    TweenMax.to($middleLine, animationSpeed, {
+      alpha: 0,
+      scaleX: 0
+    });
+    TweenMax.to($bottomLine, animationSpeed, {
+      rotation: -45,
+      y: -10
+    });
+    isVisible = true;
+
   } else {
-    $searchButton.removeClass("expanded");
-    searchVisible = false;
+    $nav.hide();
+    $nav.toggleClass("nav-expanded");
+    TweenMax.to($topLine, animationSpeed, {
+      rotation: 0,
+      y: 0
+    });
+    TweenMax.to($middleLine, animationSpeed, {
+      alpha: 1,
+      scaleX: 1
+    });
+    TweenMax.to($bottomLine, animationSpeed, {
+      rotation: 0,
+      y: 0
+    });
+    isVisible = false;
+
   }
 }
 
-$searchTrigger.on("click", searchBar);
-$navA.on("click", searchBar);
+
+$burger.on("click", mouseClick);
+// close the mobile menu when menu link is clicked
+$navA.on("click", mouseClick);
+
+// ============================ //
+//              END             //
+// ============================ //
+
+
+
+// ============================ //
+//         CONTACT FORM         //
+// ============================ //
 
 
 var $contactForm = $('#contact-form');
@@ -61,6 +91,15 @@ function formToggle() {
 $formTrigger.on("click", formToggle);
 $formExit.on("click", formToggle);
 
+// ============================ //
+//              END             //
+// ============================ //
+
+
+
+// ============================ //
+//        SCROLL-EFFECTS        //
+// ============================ //
 
 
 $(document).ready(function() {
@@ -74,143 +113,151 @@ $(document).ready(function() {
   });
 });
 
-//scrollMagic-aboutcircle
 
-// if (!is_touch_device()) {
-//     // Start ScrollMagic code
-//
-//     var $circle = $('#about-circle');
-//
-//     var controller = new ScrollMagic();
-//
-//     var timeline = new TimelineMax();
-//
-//     var tween = TweenMax.to($circle, 1, {className: "+=circle-2"});
-//     var tween2 = TweenMax.to($circle, 1, {className: "+=circle-3"});
-//     var tween3 = TweenMax.to($circle, 1, {className: "+=circle-4"});
-//     var tween4 = TweenMax.to($circle, 1, {className: "+=circle-5"});
-//
-//     var scene = new ScrollMagic.Scene({
-//       triggerElement: "#about-circle-trigger"
-//       duration: 1000px;
-//     });
-//
-//     timeline.add(tween1).add(tween2).add(tween3).add(tween4);
-//     scene.setTween(timeline)
-//     scene.addTo(controller);
-// }
-// function is_touch_device() {
-//   return 'ontouchstart' in window // works on most browsers
-//       || 'onmsgesturechange' in window; // works on ie10
-// };
-$(function(){
+$(function() {
 
-var controller = new ScrollMagic.Controller({container: "#site-wrapper"});
+  var controller = new ScrollMagic.Controller({container:'#site-wrapper'});
 
-var $container1 = $('#about-main');
-var height1 = $container1.outerHeight();
-var duration1 = height1;
-var width1 = $container1.outerWidth();
+  var $container1 = $('#about-main');
+  var height1 = $container1.innerHeight();
+  var duration1 = height1;
+  var width1 = $container1.innerWidth();
 
-function pepx(percentX){
-  pixelX = width1 * percentX / 100;
-  return pixelX;
-}
-function pepy(percentY){
-  pixelY = height1 * percentY / 100;
-  return pixelY;
-}
+  function pepx(percentX) {
+    pixelX = width1 * percentX / 100;
+    return pixelX;
+  }
 
-var circlePath = [{x:pepx(70), y:pepy(27)}, {x:pepx(100), y:pepy(55)},{x:pepx(70), y:pepy(75)}, {x:pepx(10), y:pepy(90)}];
+  function pepy(percentY) {
+    pixelY = height1 * percentY / 100;
+    return pixelY;
+  }
 
-console.log(circlePath);
+  var circlePath = [{
+    x: pepx(70),
+    y: pepy(27)
+  }, {
+    x: pepx(100),
+    y: pepy(55)
+  }, {
+    x: pepx(70),
+    y: pepy(75)
+  }, {
+    x: pepx(10),
+    y: pepy(90)
+  }];
 
-var tween = TweenMax.to('#about-circle', 0.5, {
-   bezier:{curviness:1.25, values:circlePath, autoRotate:true},
-   ease: Power1.easeOut,
-   width: 100,
-   height: 100,
-   force3D:true,
-   rotationZ:0.01   
- });
+  console.log(circlePath);
 
- // Create the Scene and trigger when visible
- var scene = new ScrollMagic.Scene({
-   triggerElement: '#about-circle-trigger',
-   offset: 300,
-   duration: duration1 /* How many pixels to scroll / animate */
- })
- .setTween(tween)
- .addIndicators({name: height1})
- .addTo(controller);
+  var tween = TweenMax.to('#about-circle', 0.5, {
+    bezier: {
+      curviness: 1.25,
+      values: circlePath
+    },
+    ease: Power2.easeOut,
+    width: 200,
+    height: 200,
+    force3D: true,
+    autoRound: false,
+  });
+
+  // Create the Scene and trigger when visible
+  var scene = new ScrollMagic.Scene({
+      triggerElement: '#about-circle-trigger',
+      offset: 300,
+      duration: duration1 /* How many pixels to scroll / animate */
+    })
+    .setTween(tween)
+    .addIndicators({
+      name: height1
+    })
+    .addTo(controller);
 
 
- var timeline = new TimelineMax();
+  var timeline = new TimelineMax();
 
- var tween2 = TweenMax.to('#quote-text', 1, {
+  var tween2 = TweenMax.to('#quote-text', 1, {
     opacity: 1,
     ease: Power1.easeOut
   });
 
   var tween1 = TweenMax.to('#quote-trigger', 0.5, {
-     className:"+=expanded-quote"
-   });
+    className: "+=expanded-quote"
+  });
 
-timeline.add(tween1).add(tween2);
+  timeline.add(tween1).add(tween2);
 
- var scene2 = new ScrollMagic.Scene({
-   triggerElement: '#quote-trigger',
-   reverse: false,
- })
- .setTween(timeline)
- .addIndicators({name:'quote'})
- .addTo(controller);
-
-
- var parallaxTimeline = new TimelineMax();
-
+  var scene2 = new ScrollMagic.Scene({
+      triggerElement: '#quote-trigger',
+      reverse: false,
+    })
+    .setTween(timeline)
+    .addIndicators({
+      name: 'quote'
+    })
+    .addTo(controller);
 
 
- var parallax1 = TweenMax.fromTo('#events .main-content .p-layer-1', 1, {
-   y: +100,
-   ease: Linear.easeNone
- },
-  {
+  var parallaxTimeline = new TimelineMax();
+
+
+
+  var parallax1 = TweenMax.fromTo('#events .main-content .p-layer-1', 1, {
+    y: +100,
+    ease: Linear.easeNone
+  }, {
     y: -100,
     ease: Linear.easeNone
-  },0);
+  }, 0);
 
   var parallax2 = TweenMax.fromTo('#events .main-content .p-layer-2', 1, {
-     y: +150,
-     ease: Linear.easeNone
-   },
-   {
-      y: -150,
-      ease: Linear.easeNone
-    },"-=1");
+    y: +150,
+    ease: Linear.easeNone
+  }, {
+    y: -150,
+    ease: Linear.easeNone
+  }, "-=1");
 
-   var parallax3 = TweenMax.fromTo('#events .main-content .p-layer-3', 1, {
-     y: +200,
-     ease: Linear.easeNone
-   },
-    {
-      y: -200,
-      ease: Linear.easeNone
-    },"-=1");
+  var parallax3 = TweenMax.fromTo('#events .main-content .p-layer-3', 1, {
+    y: +200,
+    ease: Linear.easeNone
+  }, {
+    y: -200,
+    ease: Linear.easeNone
+  }, "-=1");
 
 
 
-parallaxTimeline.add(parallax1).add(parallax2).add(parallax3);
+  parallaxTimeline.add(parallax1).add(parallax2).add(parallax3);
 
- var parallaxScene = new ScrollMagic.Scene({
-   triggerElement: '#parallax-trigger',
-   duration: $('#events').outerHeight()
+  var parallaxScene = new ScrollMagic.Scene({
+      triggerElement: '#parallax-trigger',
+      duration: $('#events').outerHeight()
 
- })
- .setTween(parallaxTimeline)
- .addIndicators({name:'parallax'})
- .addTo(controller);
+    })
+    .setTween(parallaxTimeline)
+    .addIndicators({
+      name: 'parallax'
+    })
+    .addTo(controller);
 
-
- // Add debug indicators fixed on right side
 });
+// ============================ //
+//              END             //
+// ============================ //
+
+
+// ============================ //
+//             VIDEO            //
+// ============================ //
+
+$(document).ready(function() {
+  console.log("fire");
+  $('#video-back').vidbacking({
+    'masked': true
+  });
+});
+
+// ============================ //
+//              END             //
+// ============================ //
